32dc14364368ea0218cb2a64c017ba289875450e
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-12-27 22:42:17 +0530

    added middleware to manage page visibility

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,8 +4,10 @@ import axios from "axios";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const ProfilePage = () => {
+  const [data, setData] = useState();
   const router = useRouter();
   const logout = async () => {
     try {
@@ -19,6 +21,12 @@ const ProfilePage = () => {
     }
   };
 
+  const getUserDetails = async () => {
+    const response = await axios.get("/api/users/me");
+    console.log(response.data);
+    setData(response.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
@@ -26,12 +34,23 @@ const ProfilePage = () => {
 
       <p>ProfilePage</p>
       <hr />
+      <h2>
+        {data ? <Link href={`/profile/${data}`}>{data}</Link> : "no data available!"}
+      </h2>
       <button
         onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700
       text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-500 mt-4 hover:bg-green-700
+      text-white font-bold py-2 px-4 rounded"
+      >
+        Get user details
       </button>
     </div>
   );
